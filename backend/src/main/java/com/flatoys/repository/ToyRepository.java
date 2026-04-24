7e9e491ec8489481dff900d53ec2aaff5c8140f7
andressa-ferreira <112990194+andressa-ferreira@users.noreply.github.com>
2026-04-24 02:03:18 -0300

    Update ToyRepository.java

--- backend/src/main/java/com/flatoys/repository/ToyRepository.java
+++ backend/src/main/java/com/flatoys/repository/ToyRepository.java
@@ -1,6 +1,6 @@
-package com.flatoys.repository;
+package com.toystore.repository;
 
-import com.flatoys.model.Toy;
+import com.toystore.model.Toy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
