fc88a14d15686e844575469fc6ed583098c05276
andressa-ferreira <112990194+andressa-ferreira@users.noreply.github.com>
2026-04-24 01:52:06 -0300

    Update FlatoysApplication.java

--- backend/src/main/java/com/flatoys/FlatoysApplication.java
+++ backend/src/main/java/com/flatoys/FlatoysApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlatoysApplication {
+public class ToystoreApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FlatoysApplication.class, args);
+        SpringApplication.run(ToystoreApplication.class, args);
     }
 }
